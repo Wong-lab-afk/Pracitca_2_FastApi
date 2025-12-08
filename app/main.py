3865e51a0f9a87836497b243510ef58402b0e516
Jesus Guadalupe Wong Camacho <159496734+Wong-lab-afk@users.noreply.github.com>
2025-12-08 13:18:02 -0700

    Update main.py

--- app/main.py
+++ app/main.py
@@ -2,7 +2,6 @@ from fastapi import FastAPI, Depends, HTTPException
 from pydantic import BaseModel 
 from sqlalchemy.orm import Session
 from typing import List
-
 from app.db import engine, Base, get_db
 from app import models, schemas
 from app.services.api_client import fetch_city_info
@@ -10,22 +9,21 @@ from app.services.vector_service import get_vector, calculate_similarity
 
 # Crear tablas en BD al inicio (para desarrollo)
 Base.metadata.create_all(bind=engine)
-
 app = FastAPI(title="Lead Service")
 
-# 1. Health Check [cite: 126]
+# 1. Health Check 
 @app.get("/health")
 def health_check():
     return {"status": "ok"}
 
-# 2. Crear Lead [cite: 110]
+# 2. Crear Lead
 @app.post("/api/leads", response_model=models.LeadResponse)
 async def create_lead(lead: models.LeadCreate, db: Session = Depends(get_db)):
-    # A. Llamada a API externa (asíncrona) [cite: 113]
+    # A. Llamada a API externa (asíncrona) 
     city_info = await fetch_city_info(lead.city)
     print(f"Información externa para {lead.city}: {city_info}")
     
-    # B. Guardar en Postgres [cite: 114]
+    # B. Guardar en Postgres 
     db_lead = schemas.Lead(
         name=lead.name,
         email=lead.email,
@@ -38,13 +36,13 @@ async def create_lead(lead: models.LeadCreate, db: Session = Depends(get_db)):
     db.refresh(db_lead)
     return db_lead
 
-# 3. Listar Leads [cite: 116]
+# 3. Listar Leads 
 @app.get("/api/leads", response_model=List[models.LeadResponse])
 def read_leads(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
     leads = db.query(schemas.Lead).offset(skip).limit(limit).all()
     return leads
 
-# 4. Buscar Similitud [cite: 119]
+# 4. Buscar Similitud 
 class SearchQuery(BaseModel):
     query: str
 
